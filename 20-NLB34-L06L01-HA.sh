e06ac928e5c7bea5689759c26c9cd0b547e1e565
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-04-18 04:15:42 -0700

    Update 20-NLB34-L06L01-HA.sh

--- 20-NLB34-L06L01-HA.sh
+++ 20-NLB34-L06L01-HA.sh
@@ -4,10 +4,12 @@ PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE/main/TCP/main.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/TCP/main.conf
 sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE/main/HA/nginx.conf
-sudo scp /tmp/nginx.conf nginx:/etc/nginx/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HA/nginx.conf
+
+#### NEED TO GET NEW UPDATED R34 nginx.conf stuff like mgmt{} context
+sudo scp /tmp/nginx.conf nginx:/etc/nginx/LAB_nginx.conf
 
 #Add nginx license to nginx2 lab system, required for keepalived setup
 sudo scp /tmp/nginx-one-eval.crt nginx2:/etc/ssl/nginx/nginx-repo.crt
@@ -16,10 +18,10 @@ sudo scp /tmp/nginx-one-eval.jwt nginx2:/etc/nginx/license.jwt
 
 #This tcp directory created in lab 5, so it should carry on
 sudo ssh nginx mkdir /etc/nginx/tcp
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE/main/HA/tcp_lb.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HA/tcp_lb.conf
 sudo scp /tmp/tcp_lb.conf nginx:/etc/nginx/tcp
 
 #Add nginx servers to /etc/hosts file
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE/main/HA/hosts
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HA/hosts
 sudo scp /tmp/hosts nginx:/etc/hosts
 sudo scp /tmp/hosts nginx2:/etc/hosts
