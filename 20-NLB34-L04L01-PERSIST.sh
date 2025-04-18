d6a58be079d0392ba5e1e4519162b2b8f5637dc7
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-04-18 04:13:21 -0700

    Update 20-NLB34-L04L01-PERSIST.sh

--- 20-NLB34-L04L01-PERSIST.sh
+++ 20-NLB34-L04L01-PERSIST.sh
@@ -4,5 +4,5 @@ PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE/main/PERSIST/main.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/PERSIST/main.conf
 sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
