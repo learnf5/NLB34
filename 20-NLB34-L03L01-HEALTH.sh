f30e3196f91cc1fc793bc88bd1167f77ea41f7a5
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-04-18 04:13:08 -0700

    Update 20-NLB34-L03L01-HEALTH.sh

--- 20-NLB34-L03L01-HEALTH.sh
+++ 20-NLB34-L03L01-HEALTH.sh
@@ -4,5 +4,5 @@ PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE/main/HEALTH/main.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/HEALTH/main.conf
 sudo scp /tmp/main.conf nginx:/etc/nginx/conf.d/
