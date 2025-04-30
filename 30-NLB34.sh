996e383c3b6698308cbc73e4d822ab71e74d4e1d
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-04-30 09:29:07 -0700

    Update 30-NLB34.sh
    removed licenses they are now in 10xxx script

--- 30-NLB34.sh
+++ 30-NLB34.sh
@@ -1,16 +1,3 @@
-# install 30 day nginx one trial licenses from Salesforce - EXPIRES May 22, 2025
-set +x
-curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-May-22-2025/nginx-one-eval.{crt,key,jwt}
-echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-May-22-2025/nginx-one-eval.{crt,key,jwt}
-set -x
-until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
-until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
-until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
-
-# Add nginx license to nginx2 lab system, required for keepalived setup
-sudo scp /tmp/nginx-one-eval.crt nginx2:/etc/ssl/nginx/nginx-repo.crt
-sudo scp /tmp/nginx-one-eval.key nginx2:/etc/ssl/nginx/nginx-repo.key
-sudo scp /tmp/nginx-one-eval.jwt nginx2:/etc/nginx/license.jwt
 
 # restart NGINX
 sudo ssh nginx sudo systemctl stop nginx
